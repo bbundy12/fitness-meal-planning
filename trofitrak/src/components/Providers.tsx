fa66d43a37a80acbcc7399124d868225d77f88b2
Brendan <brendanbundy1@gmail.com>
2026-01-03 23:32:24 -0700

    Fixes so app opens

--- trofitrak/src/components/Providers.tsx
+++ trofitrak/src/components/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import { httpBatchLink } from "@trpc/client";
 import type { ReactNode } from "react";
 import { useState } from "react";
@@ -20,12 +21,14 @@ export function Providers({ children }: { children: ReactNode }) {
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
-    }),
+    })
   );
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
+    <SessionProvider>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </trpc.Provider>
+    </SessionProvider>
   );
 }
